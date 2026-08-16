f14370237da7f8bc3242e647e2fe428212540a99
Nick Wellnhofer <wellnhofer@aevum.de>
2026-08-16 15:25:46 +0200

    runtime: Don't warn about function pointer casts

--- runtime/common/charmonizer.c
+++ runtime/common/charmonizer.c
@@ -8904,7 +8904,9 @@ S_add_compiler_flags(struct chaz_CLI *cli) {
              */
             chaz_CFlags_append(extra_cflags, "-pedantic");
         }
-        chaz_CFlags_append(extra_cflags, "-Wall -Wextra -Wno-variadic-macros");
+        chaz_CFlags_append(extra_cflags,
+            "-Wall -Wextra -Wno-variadic-macros -Wno-cast-function-type "
+            "-Wno-cast-function-type-mismatch");
         if (strcmp(chaz_CLI_strval(cli, "host"), "perl") == 0) {
             chaz_CFlags_append(extra_cflags, "-DPERL_GCC_PEDANTIC");
         }
